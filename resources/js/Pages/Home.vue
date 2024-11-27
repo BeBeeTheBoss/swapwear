0db562124350ef25c3ed4869c5c030c3cd52a87b
ophirbackend01 <ophir.backend01@gmail.com>
2024-11-27 15:13:09 +0630

    ui setup

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,19 @@
 <template>
 
-    <div>
-        Hello You're great BeBee
+    <div class="container">
+        Hello World !
     </div>
 
 </template>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color:white;
+    font-weight: 600;
+}
+</style>
