ec5b8a57dfb3c76ed73bb1e40d91cd3483c83e5d
ophirbackend01 <ophir.backend01@gmail.com>
2024-12-19 14:52:49 +0630

    bug fix logo image

--- resources/js/Pages/Layouts/Header.vue
+++ resources/js/Pages/Layouts/Header.vue
@@ -3,7 +3,7 @@
         <div class="container p-2 flex items-center justify-between text-white">
             <brand class="h5 col-6">
                 <div class="flex items-center">
-                    <img src="../../../../public/images/bag.png" class="me-2" alt="" style="width:19px;height:19px">
+                    <img :src="logoImage" class="me-2" alt="" style="width:19px;height:19px">
                     Vintage
                 </div>
             </brand>
@@ -16,6 +16,7 @@
 
 <script setup>
 import Search from './Search.vue';
+import logoImage from '../../../../public/images/bag.png'
 </script>
 
 <style></style>
