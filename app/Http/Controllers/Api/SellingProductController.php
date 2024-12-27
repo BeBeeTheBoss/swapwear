705a93dbe205617481003c99974652c59ab979a2
ophirbackend01 <ophir.backend01@gmail.com>
2024-12-27 12:23:30 +0630

    add crud api for selling products

--- app/Http/Controllers/Api/SellingProductController.php
+++ app/Http/Controllers/Api/SellingProductController.php
@@ -5,32 +5,38 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\SellingProduct;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\SellingProductRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\SellingProductRequest;
 use App\Http\Resources\SellingProductResource;
 
 class SellingProductController extends Controller
 {
     public function __construct(protected SellingProduct $model) {}
 
-    public function index(Request $request,$id = null){
-        $data = $this->model->when($id,function($query) use ($id){
-            $query->where('id',$id);
-        })->when($request->query,function($query) use ($request){
-            if($request->query('with') == 'user'){
-                $query->with('user');
-            }
+    public function index(Request $request, $id = null)
+    {
+        $data = $this->model->when($id, function ($query) use ($id) {
+            $query->where('id', $id);
+        })->when($request->query, function ($query) use ($request) {
 
-            if($request->query('with') == 'sub-category'){
-                $query->with('sub_category');
-            }
+            $relations = explode(',', $request->query('with'));
 
+            foreach ($relations as $relation) {
+
+                if ($relation == 'sub-category') {
+                    $query->with('sub_category');
+                } else if ($relation == 'user') {
+                    $query->with('user');
+                }
+            }
         })->get();
 
-        return sendResponse($data,200);
+        return sendResponse(SellingProductResource::collection($data), 200);
     }
 
-    public function store(SellingProductRequest $request){
+    public function store(SellingProductRequest $request)
+    {
 
         $selling_product = $this->model->create($request->images ? $this->toArray($request->except('images')) : $this->toArray($request));
 
@@ -41,24 +47,83 @@ class SellingProductController extends Controller
                 $imageName = storeImage($imageFile, '/product_images/'); //store image to destination folder
                 $selling_product->images()->create(['name' => $imageName]);
             }
-
         }
 
         $selling_product->save();
 
-        return sendResponse(new SellingProductResource($selling_product),200);
+        return sendResponse(new SellingProductResource($selling_product), 200);
     }
 
-    private function toArray($request){
+    public function update(SellingProductRequest $request)
+    {
+        $request->validate([
+            'id' => 'required'
+        ]);
+
+        $selling_product = $this->model->find($request->id);
+        if (!$selling_product) {
+            return sendResponse(null, 404, 'Selling product not found');
+        }
+
+        //update other data except images
+        $selling_product->update($request->images ? $this->toArray($request->except('images')) : $this->toArray($request));
+
+        //check deleted images exist or not and if exists delete from db and storage folder
+        if ($request->deleted_images) {
+            $deleted_image_names = $selling_product->images()->whereIn('id', $request->deleted_images)->pluck('name'); //collect deleted image names form db
+            foreach ($deleted_image_names as $din) {
+                Storage::delete('public/product_images/' . $din);  //delete from storage folder
+            }
+            $deleted_image_names = $selling_product->images()->whereIn('id', $request->deleted_images)->delete(); //delete from db
+        }
+
+        if ($request->file('images')) {
+
+            $imageFiles = $request->file('images');
+
+            foreach ($imageFiles as $imageFile) {
+                if (is_file($imageFile)) {      //check new images are included or not
+                    $imageName = storeImage($imageFile, '/product_images/'); //store image to destination folder
+                    $selling_product->images()->create(['name' => $imageName]);
+                }
+            }
+        }
+
+        return sendResponse(new SellingProductResource($selling_product), 200, 'Selling product updated success');
+    }
+
+    public function destroy(Request $request)
+    {
+        $request->validate([
+            'id' => 'required'
+        ]);
+
+        $selling_product = $this->model->find($request->id);
+        if(!$selling_product){
+            return sendResponse(null,404,'Selling product already deleted');
+        }
+
+        $deleted_image_names = $selling_product->images()->pluck('name');
+        foreach ($deleted_image_names as $din) {
+            Storage::delete('public/product_images/' . $din);
+        }
+        $deleted_image_names = $selling_product->images()->delete();
+
+        $selling_product->delete();
+
+        return sendResponse(null, 204, 'Selling product deleted success');
+    }
+
+    private function toArray($request)
+    {
         return [
             'user_id' => Auth::user()->id,
-            'sub_category_id' => $request->sub_category_id,
-            'name' => $request->name,
-            'description' => $request->description,
-            'condition' => $request->condition,
-            'price' => $request->price,
-            'quantity' => $request->quantity
+            'sub_category_id' => (int) $request['sub_category_id'],
+            'name' => $request['name'],
+            'description' => $request['description'],
+            'condition' => $request['condition'],
+            'price' => (int) $request['price'],
+            'quantity' => $request['quantity']
         ];
     }
-
 }
--- app/Http/Resources/SellingProductResource.php
+++ app/Http/Resources/SellingProductResource.php
@@ -14,6 +14,23 @@ class SellingProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'sub_category_id' => $this->sub_category_id,
+            'sub_category' => new SubCategoryResource($this->whenLoaded('sub_category')),
+            'name' => $this->name,
+            'description' => $this->description,
+            'condition' => $this->condition,
+            'quantity' => $this->quantity,
+            'price' => $this->price,
+            'status' => $this->selling,
+            'is_active' => $this->is_active,
+            'images' => SellingProductImageResource::collection($this->images),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
